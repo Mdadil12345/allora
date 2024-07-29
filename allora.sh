2a4176152c029518f255e8789896493f847685b7
Mdadil <121780915+Mdadil12345@users.noreply.github.com>
2024-07-30 01:33:58 +0530

    Create allora.sh

---
+++ allora.sh
@@ -0,0 +1,7 @@
+echo "# allora" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Mdadil12345/allora.git
+git push -u origin main
